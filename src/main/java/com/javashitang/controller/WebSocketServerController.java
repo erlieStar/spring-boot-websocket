3cd2e49cbaea768b2e0ff9a286b92c7dba9cc5be
erlie <290059742@qq.com>
2019-11-27 00:04:49 +0800

    增加注解

--- src/main/java/com/javashitang/controller/WebSocketServerController.java
+++ src/main/java/com/javashitang/controller/WebSocketServerController.java
@@ -1,4 +1,29 @@
 package com.javashitang.controller;
 
+@ServerEndpoint("/websocket")
 public class WebSocketServerController {
+
+    /**
+     * 收到消息调用的方法
+     */
+    @OnMessage
+    public void onMessage(String message) {
+
+    }
+
+    /**
+     * 建立连接调用的方法
+     */
+    @OnOpen
+    public void onOpen() {
+        System.out.println("Client connected");
+    }
+
+    /**
+     * 关闭连接调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        System.out.println("Connection closed");
+    }
 }
