fe6f861f05ac504fee86506869056b41301928ac
lilimin <290059742@qq.com>
2019-11-27 19:58:49 +0800

    完成demo的演示

--- src/main/java/com/javashitang/config/WebSocketConfig.java
+++ src/main/java/com/javashitang/config/WebSocketConfig.java
@@ -1,10 +1,10 @@
 package com.javashitang.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-@Component
+@Configuration
 public class WebSocketConfig {
 
     @Bean
--- src/main/java/com/javashitang/controller/WebSocketServerController.java
+++ src/main/java/com/javashitang/controller/WebSocketServerController.java
@@ -1,29 +1,45 @@
 package com.javashitang.controller;
 
-@ServerEndpoint("/websocket")
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@Component
+@ServerEndpoint("forshow/websocket/{sid}")
 public class WebSocketServerController {
 
-    /**
-     * 收到消息调用的方法
-     */
+    // 收到消息调用的方法
     @OnMessage
-    public void onMessage(String message) {
-
+    public void onMessage(Session session, String message) {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * 建立连接调用的方法
-     */
+    // 建立连接调用的方法
     @OnOpen
-    public void onOpen() {
+    public void onOpen(@PathParam("sid") String sid) {
         System.out.println("Client connected");
     }
 
-    /**
-     * 关闭连接调用的方法
-     */
+    // 关闭连接调用的方法
     @OnClose
     public void onClose() {
         System.out.println("Connection closed");
+    }
+
+    // 传输消息错误调用的方法
+    @OnError
+    public void OnError(Throwable error) {
+        System.out.println("Connection error");
     }
 }
